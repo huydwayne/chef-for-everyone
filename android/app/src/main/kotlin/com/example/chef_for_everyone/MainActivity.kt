5ade0195de3d127111dd9a5beb82b37c6a8c253b
huydwayne <anhhhuydayne@gmail.com>
2021-05-28 08:09:30 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/chef_for_everyone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chef_for_everyone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/onboarding.dart
@@ -0,0 +1,84 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+import 'package:material_kit_flutter/constants/Theme.dart';
+
+class Onboarding extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(children: <Widget>[
+      Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/img/onboard-background-free.jpeg"),
+                  fit: BoxFit.cover))),
+      Padding(
+        padding:
+            const EdgeInsets.only(top: 73, left: 32, right: 32, bottom: 16),
+        child: Container(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 48.0),
+                      child: Text.rich(TextSpan(
+                        text: "Chef for Everyone",
+                        style: TextStyle(color: Colors.white, fontSize: 58),
+                      )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 24.0),
+                      child: Text("Đầu bếp là của bạn.",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w200)),
+                    ),
+                  ],
+                ),
+                // Padding(
+                //   padding: const EdgeInsets.symmetric(vertical: 42.0),
+                //   child: Row(children: <Widget>[
+                //     Image.asset("assets/img/logo-ios.png", scale: 2.6),
+                //     SizedBox(width: 30.0),
+                //     Image.asset("assets/img/logo-android.png", scale: 2.6)
+                //   ]),
+                // ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 60.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: FlatButton(
+                      textColor: Colors.white,
+                      color: MaterialColors.info,
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/home');
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4.0),
+                      ),
+                      child: Padding(
+                          padding: EdgeInsets.only(
+                              left: 16.0, right: 16.0, top: 12, bottom: 12),
+                          child: Text("Tham gia",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16.0))),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      )
+    ]));
+  }
+}
